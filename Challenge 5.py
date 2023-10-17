cfe1284f1cfc213d65bf61eedab2ed8b951c0359
mr5rafik786 <146173728+mr5rafik786@users.noreply.github.com>
2023-10-17 15:15:45 +0530

    Update and rename main.py to Challenge 5.py

--- Challenge 5.py
+++ Challenge 5.py
@@ -1,4 +1,4 @@
-po"""
+"""
 Write a function called linear_search_product that takes the list of products and a target product
 name as input. The function should perform a linear search to find the target product in the list and
 return a list of indices of all occurrences of the product if found, or an empty list if the product is not
